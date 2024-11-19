1087f37e8abf63a349c2250e051315dcf25a44c3
Eduardo Aguilar <dante.aguilar41@gmail.com>
2024-11-19 14:05:20 -0600

    chore: add files after format

--- .husky/pre-commit-scripts/eslint.sh
+++ .husky/pre-commit-scripts/eslint.sh
@@ -11,8 +11,13 @@ if [ $? -ne 0 ]; then
 fi
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 git add --all
 
 =======
 >>>>>>> b043e08 (chore: replace precommit to use husky)
+=======
+git add --all
+
+>>>>>>> d2ff610 (chore: add files after format)
 exit 0
